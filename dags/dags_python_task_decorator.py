210fdf7f6447712bc7a32b6a970e8630f413ec49
HyejungLee52 <lee.hyejung.52@gmail.com>
2024-03-25 15:28:12 +0900

    dags/dags_python_import_func.py modify

--- dags/dags_python_task_decorator.py
+++ dags/dags_python_task_decorator.py
@@ -1,5 +1,6 @@
 import pendulum
-from airflow.decorators import DAG, task
+from airflow.models.dag import DAG
+from airflow.decorators import task
 
 with DAG(
     dag_id="dags_python_task_decorator",
