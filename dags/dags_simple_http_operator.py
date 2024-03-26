8ce35abd6255b30b5ad7730b8bf989d4cf5ee802
HyejungLee52 <lee.hyejung.52@gmail.com>
2024-03-26 16:23:10 +0900

    dags/dags_simple_http_operator added

---
+++ dags/dags_simple_http_operator.py
@@ -0,0 +1,34 @@
+import pendulum
+from airflow.models.dag import DAG
+from airflow.operators.bash import BashOperator
+from airflow.providers.http.operators.http import SimpleHttpOperator
+from airflow.decorators import task
+
+with DAG(
+    dag_id="dags_simple_http_operator",
+    schedule=None,
+    start_date=pendulum.datetime(2024, 3, 26, tz="Asia/Seoul"),
+    catchup=False
+) as dag:
+    
+    '''서울시 공공자전거 실시간 이용정보'''
+    tb_cycle_station_info = SimpleHttpOperator(
+        task_id ='tb_cycle_station_info',
+        http_conn_id = 'openapi.seoul.go.kr',
+        endpoint='{{var.value.apikey_openapi_seoul.go.kr}}/json/bikeList/1/5/',
+        method='GET',
+        header={'Content-Type' : 'application/json',
+                'charset':'utf-8',
+                'Accept':'*/*'
+                }
+    )
+    
+    @task(task_id='python_2')
+    def python_2(**kwargs):
+        ti = kwargs['ti']
+        result_lst = ti.xcom_pull(task_ids='tb_cycle_station_info')
+        import json
+        from pprint import pprint
+        pprint(json.loads(result_lst))
+
+    tb_cycle_station_info >> python_2()
