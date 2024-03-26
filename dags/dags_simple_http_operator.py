de52a65ac46ddc286419d69db1598091e9a6979c
HyejungLee52 <lee.hyejung.52@gmail.com>
2024-03-26 16:56:13 +0900

    dags/dags_trigger_dag_run_operato added

--- dags/dags_simple_http_operator.py
+++ dags/dags_simple_http_operator.py
@@ -15,7 +15,7 @@ with DAG(
     tb_cycle_station_info = SimpleHttpOperator(
         task_id ='tb_cycle_station_info',
         http_conn_id = 'openapi.seoul.go.kr',
-        endpoint='{{var.value.apikey_openapi_seoul.go.kr}}/json/bikeList/1/5/',
+        endpoint='{{var.value.apikey_openapi_seoul_go_kr}}/json/bikeList/1/5/',
         method='GET',
         header={'Content-Type' : 'application/json',
                 'charset':'utf-8',
